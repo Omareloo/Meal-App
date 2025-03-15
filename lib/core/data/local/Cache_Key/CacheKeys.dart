fd787e5b5a4f16c5a6003a155f599de1e9c2f7fc
joeshaker <youssefshaker296@gmail.com>
2025-03-15 17:57:25 +0200

    AddSharedPreference&HiveProfileModel

---
+++ lib/core/data/local/Cache_Key/CacheKeys.dart
@@ -0,0 +1,7 @@
+abstract class CacheKeys {
+  static const String isFirstTime = 'isFirstTime';
+  static const String currentLanguageCode = 'currentLanguageCode';
+  static const String currentLanguage = 'currentLanguage';
+  static const String token = "token";
+  static const String id = "id";
+}
--- lib/core/di/db_injection.dart
+++ lib/core/di/db_injection.dart
@@ -1,5 +1,6 @@
 import 'package:get_it/get_it.dart';
 import '../../features/auth/data/data_source/auth_datasource.dart';
+import '../data/local/Profile/ProfileService.dart';
 import '../data/local/shared_preferences_service.dart';
 import '../../features/auth/data/repositories/auth_repository.dart';
 import '../../features/auth/domain/repositories/auth_repo.dart';
@@ -8,6 +9,8 @@ import '../../features/auth/domain/repositories/auth_repo.dart';
 final sl = GetIt.instance;
 
 Future <void> init()async{
+  sl.registerSingleton<ProfileService>(ProfileService());
+  sl.registerSingleton<SharedPreferencesHelper>(SharedPreferencesHelper());
 //Remote data source
 sl.registerLazySingleton<DataSource>(
         ()=>DataSourceImpl(auth: sl.call(), firestore: sl.call()));
